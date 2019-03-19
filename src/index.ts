67975c8b52b978b5995499d4b4ca1cfefb6b4deb
TMCNEILL28 <48483907+TMCNEILL28@users.noreply.github.com>
2019-03-19 17:22:10 +0000

    More Javascript changes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
 import './styles.css';
 
-const billAmount = document.getElementById('billAmount');
-let currentTipPercent = .10;
+const billAmount = <HTMLInputElement>document.getElementById('billInput');
+let currentTipPercent = '10%';
 
-const tipButton = document.querySelectorAll('.btn btn-secondary');
+const tipButtons = document.querySelectorAll('.btn-secondary');
 
 billAmount.addEventListener('input', function (evt) {
 
     //update Values
+    document.getElementById('billAmount2').innerHTML = this.value;
+    alert(this.value);
 
+});
+
+tipButtons.forEach((btn, idx) => {
+    const el = btn as HTMLDivElement;
+    btn.addEventListener('click', function () {
+        if (!this.classList.contains('disabled')) {
+            this.classList.add('disabled');
+        }
+        currentTipPercent = this.innerText;
+        document.getElementById('tipPercent').innerHTML = currentTipPercent;
+
+    })
 });
 
